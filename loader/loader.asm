816ff79863f27696e3acc91658c4c3d61d11fa8c
ScSofts <3416616002@qq.com>
2020-12-29 23:11:25 +0800

    Format comments

--- loader/loader.asm
+++ loader/loader.asm
@@ -1,10 +1,13 @@
+
 [BITS 16]      ; 16 bit code generation
 [ORG 0x7C00]   ; Origin location
-%include "loader/utils.asm"
+%include "loader/utils.asm";include marco defines
 
+;=====================FAT32 Head====================
 head:
-	jmp main
-	nop;fill the space
+	jmp main					;went to the main program
+	nop							;fill the space
+
 	OEM_ID                db 		"ReUX OS",0
 	BytesPerSector        dw 		0x0200
 	SectorsPerCluster     db 		0x08
@@ -17,7 +20,7 @@ head:
 	SectorsPerTrack       dw 		0x003d
 	SectorsPerHead        dw 		0x0002
 	HiddenSectors         dd 		0x00000000
-	TotalSectors     	   dd 		0x00fe3b1f		
+	TotalSectors     	  dd 		0x00fe3b1f		
 	BigSectorsPerFAT      dd 		0x00000778
 	Flags                 dw 		0x0000
 	FSVersion             dw 		0x0000
@@ -25,7 +28,7 @@ head:
 	FSInfoSector          dw 		0x0001
 	BackupBootSector      dw 		0x0006
 
-	TIMES 12 DB 0 ;jumping to next offset
+	times 12 db 0 				;jumping to next offset
 
 	DriveNumber           db 		0x00
 	ReservedByte          db   		0x00
@@ -33,24 +36,26 @@ head:
 	VolumeID              dd 		0x3d400fbf
 	VolumeLabel           db 		"ReUX OS",0,0,0,0
 	SystemID              db 		"FAT32   "
-; Main program
+
+; ===================== Main program==================
 main:
+	;init registers
 	mov ax,cs
-	;section register
-	;address x will be ds:x
 	mov ds,ax
 	mov es,ax
 	mov ss,ax
 	mov fs,ax
 	mov sp,0x7c00
-	Clear
-	SetCursor 0,0,0
-	Puts Loading
-	SetCursor 0,1,0
 	
-end_program:
-	Pause
+	Clear						;Clear the screen
+	SetCursor 0,0,0				;Return the cursor to line 0 col 0
+	Puts Loading				;Print the string to the screen
+	SetCursor 0,1,0				;Go to next line
+
+	;main end
+	end_program:
+		Pause 					;Stop the program
 data:
 	Loading: db "Loading from hard disk...",13,0
-times 510-($-$$) db 0	; Fill the rest with zeros
-dw 0xAA55		; Boot loader signature
+times 510-($-$$) db 0			; Fill the rest with zeros
+dw 0xAA55						; Boot loader signature
